175e1f482ccf7daec45bb78ed305568e56ee24d2
jairaume <jerome.rascle@student.junia.com>
2023-01-30 14:07:46 +0100

    fix slider display

--- src/popup/js/index.js
+++ src/popup/js/index.js
@@ -1,15 +1,33 @@
-
 // This is the code that runs in the popup
 
 let checkbox = document.getElementById("checkbox").parentElement;
+let globalCont = document.getElementById("global-cont");
+let sliders = document.getElementsByClassName("slider");
+console.log(sliders);
+
+
+
 checkbox.addEventListener("change", async() => {
     var full = await chrome.storage.sync.get(['on']);
     chrome.storage.sync.set({ on: !full.on });
 
     if(!full.on){
         checkbox.classList.add("active");
+        globalCont.classList.add("active");
     }
     else{
         checkbox.classList.remove("active");
+        globalCont.classList.remove("active");
     }
 });
+
+Array.from(sliders).forEach((slider) => {
+    slider = slider.firstElementChild;
+    console.log(slider)
+    slider.addEventListener("change", async() => {
+        console.log("change on slider", slider.id, " : ", slider.value);
+        //chrome.storage.sync.set({colors[slider.id]:colors})
+    });
+    
+});
+
